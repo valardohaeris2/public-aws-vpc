c59c453df29a6c7473025a78175458429d5ec54e
Ashley Conner <108750120+valardohaeris2@users.noreply.github.com>
2023-06-05 15:40:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>4.16"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">=3.1.0"
-    }
-  }
-}
-
-#TFC Backend for tf state
- terraform {
-   cloud {
-     organization = "<org_name>"
-
-     workspaces {
-       name = "<workspace_name>"
-     }
-   }
- }
-
-# Configure the AWS Provider 
-provider "aws" {
-  region = var.region
-}
-
 # Create a VPC in us-east-2
 resource "aws_vpc" "winterfell" {
   cidr_block           = var.vpc_cidr
